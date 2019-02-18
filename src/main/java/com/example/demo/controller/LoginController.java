89ce50f468b8b69f401884208f58b4b7386e3567
guorunqi <827836028@qq.com>
2019-02-18 15:14:28 +0800

    创建项目

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.ControllerReturn;
+import com.example.demo.domain.User;
+import com.example.demo.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+
+@Controller
+public class LoginController {
+    @Autowired
+    private LoginService loginService;
+
+
+
+    @RequestMapping(value = "/login")
+    @ResponseBody
+    public ControllerReturn loginVerify(String username,String password,HttpSession session){
+        ControllerReturn controllerReturn=new ControllerReturn();
+        try {
+            User user = loginService.loginVerification(username);
+            if (user!=null&&password.equals(user.getPassword())) {
+                session.setAttribute("user", user );
+                controllerReturn.setCode("true");
+                controllerReturn.setData(user);
+                return controllerReturn;
+            } else {
+                controllerReturn.setCode("false");
+                controllerReturn.setMessage("用户名或密码不正确");
+            }
+            return controllerReturn;
+        }catch (Exception e){
+            controllerReturn.setCode("false");
+            controllerReturn.setMessage(e.toString());
+            return controllerReturn;
+        }
+
+
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.removeAttribute("user");
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.User;
+import com.example.demo.domain.UserExample;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class LoginService {
+    @Resource
+    private com.example.demo.dao.UserMapper UserMapper;
+    public User loginVerification(String username){
+        UserExample example=new UserExample();
+        example.or().andLoginNameEqualTo(username).andRoleIdEqualTo("1");
+        List<User> users=UserMapper.selectByExample(example);
+        if(users.size()>0){
+            return users.get(0);
+        }else{
+            return null;
+        }
+    }
+}
